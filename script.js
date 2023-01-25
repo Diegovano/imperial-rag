49a0b2fb06eaa17ba48bf914cb2dd5ce3a7d8b25
Diego Van Overberghe <36131808+Diegovano@users.noreply.github.com>
2023-01-25 17:16:22 +0000

    update navbar animations

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 window.onscroll = () => {
   const logo = document.getElementById("headerLogo");
   const logoBox = document.getElementById("logoBox");
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  const header = document.getElementById("header")
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
     logo.style.maxWidth = "100px";
     logoBox.style.padding = "0 10px";
+    header.style.height = `${Math.max(250 - document.documentElement.scrollTop, 100)}px`;
     for (let elem of document.getElementsByClassName("navbarLink")) {
       elem.style.height = "5vh";
     }
+
   } else {
     logo.style.maxWidth = "200px";
     logoBox.style.padding = "10px";
+    header.style.height = "250px";
     for (let elem of document.getElementsByClassName("navbarLink")) {
       elem.style.height = "7.5vh";
     }
