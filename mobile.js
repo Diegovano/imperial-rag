79d25a6b0cc221301efc0f2cf796a35f2bc663be
Diego Van Overberghe <36131808+Diegovano@users.noreply.github.com>
2023-01-26 00:59:27 +0000

    Update website according to new branding, update logos and colours. Minify committee images. Issue remains with no transition occuring during the first scroll down.

--- mobile.js
+++ mobile.js
@@ -1,21 +1,23 @@
+const logoDefMaxWidth = 200;
+
 window.onscroll = () => {
   const logo = document.getElementById("headerLogo");
   const logoBox = document.getElementById("logoBox");
   const header = document.getElementById("header")
   if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-    logo.style.maxWidth = "100px";
+    logo.style.maxWidth = "100px" /*`${Math.max(logoDefMaxWidth - document.documentElement.scrollTop, 100)}px`*/;
     logoBox.style.padding = "10px 10px";
-    header.style.height = `${Math.max(250 - document.documentElement.scrollTop, 125)}px`;
     for (let elem of document.getElementsByClassName("navbarLink")) {
       elem.style.height = "5vh";
     }
+    header.style.height = `${Math.max(320 - document.documentElement.scrollTop, 185)}px`;
 
   } else {
-    logo.style.maxWidth = "200px";
+    logo.style.maxWidth = `${logoDefMaxWidth}px`;
     logoBox.style.padding = "10px 30px";
-    header.style.height = "250px";
     for (let elem of document.getElementsByClassName("navbarLink")) {
       elem.style.height = "7.5vh";
     }
+    header.style.height = "inital";
   }
 }
